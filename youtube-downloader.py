77ee8e5ea242b09107854aa3b0a368040a373830
ziaalian <57230599+ziaalian@users.noreply.github.com>
2021-03-04 17:53:27 +0200

    updating code

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -4,7 +4,7 @@ from pytube import YouTube
 link = ('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 #link = input("please enter the video url: ")
 
-link = YouTube(link)
+video = YouTube(link)
 
-print("the video title is:{}".fomrat(link.title))
-#print(f"the video description is:\n {video.description} \n ------------------")
+print(f"the video title is:\n {video.title} \n ------------------")
+print(f"the video description is:\n {video.description} \n ------------------")
